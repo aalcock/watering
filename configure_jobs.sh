991fa764e9ab7019f6e8aa43e005f1e080efb37b
Andrew Alcock <andrew@alcock.sg>
2016-06-12 17:40:51 +0800

    Documentation of dependencies

---
+++ configure_jobs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+READ=read_environment.sh
+
+CMD=${HERE}/${READ}
+JOB="*/5 * * * * root ${CMD}"
+cat <(fgrep -v "$READ" <(crontab -l)) <(echo "$JOB") | crontab -
