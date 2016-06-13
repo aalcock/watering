e9c7c743c2e7a78928fc2ff922ed85978c2f04e2
Andrew Alcock <andrew@alcock.sg>
2016-06-13 11:18:05 +0800

    Now read_environment creates a log file

--- install_jobs.sh
+++ install_jobs.sh
@@ -3,6 +3,9 @@
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 READ=read_environment.sh
 
-CMD=${HERE}/${READ}
+CMD="${HERE}/${READ} >> /var/log/read_environment.log"
 JOB="*/5 * * * * root ${CMD}"
+
+# append the Cron command to crontab, removing any previous entry
 cat <(fgrep -v "$READ" <(crontab -l)) <(echo "$JOB") | crontab -
+
